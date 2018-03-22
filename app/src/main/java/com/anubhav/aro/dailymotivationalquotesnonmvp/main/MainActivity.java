90f9e26acf44e490d33b5d9b657fe256800541e1
anubhav <aroraanubhav@yahoo.com>
2018-03-22 11:01:20 +0530

    first stage complete

--- app/src/main/java/com/anubhav/aro/dailymotivationalquotesnonmvp/main/MainActivity.java
+++ app/src/main/java/com/anubhav/aro/dailymotivationalquotesnonmvp/main/MainActivity.java
@@ -5,8 +5,11 @@ import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,20 +25,27 @@ public class MainActivity extends AppCompatActivity {
     private JobInfo jobInfo;
     private SharedPreferences sharedPreferences;
     private static final String PREF_NAME = "SHARED_PREFS";
+    private Typeface custom_font;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
 
         initViews();
         sharedPreferences = getSharedPreferences(PREF_NAME, this.MODE_PRIVATE);
 
-        jobSchedulerService = new JobSchedulerService(sharedPreferences);
+        jobSchedulerService = new JobSchedulerService();
 
         jobInfo = new JobInfo.Builder(1, new ComponentName(this, JobSchedulerService.class))
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                .setPeriodic(3000)
+                .setPeriodic(1000 * 60 * 60 * 24)
                 .build();
 
         jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
@@ -47,8 +57,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateView() {
         if (sharedPreferences != null) {
-            if (sharedPreferences.getString("error_message", null) != null) {
+            if (sharedPreferences.getString("error_message", null) == null) {
                 textView.setText(sharedPreferences.getString("quote_title", null));
+                textView.setTypeface(custom_font);
+                //textView.setText(R.string.test_string);
+//                Picasso.with(this)
+//                        .load(sharedPreferences.getString("quote_url", null))
+//                        .into(imageView);
             }
         }
     }
@@ -56,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         textView = findViewById(R.id.quote_text);
         imageView = findViewById(R.id.quote_image);
+        custom_font = Typeface.createFromAsset(getAssets(), "fonts/brody.ttf");
     }
 
 }
--- app/src/main/java/com/anubhav/aro/dailymotivationalquotesnonmvp/util/JobSchedulerService.java
+++ app/src/main/java/com/anubhav/aro/dailymotivationalquotesnonmvp/util/JobSchedulerService.java
@@ -11,7 +11,6 @@ import com.anubhav.aro.dailymotivationalquotesnonmvp.model.Quotes;
 import com.anubhav.aro.dailymotivationalquotesnonmvp.rest.ApiInterface;
 import com.anubhav.aro.dailymotivationalquotesnonmvp.rest.ApiService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -27,21 +26,16 @@ public class JobSchedulerService extends JobService {
     private static List<Quotes> quotesList;
     private static final String CATEGORY = "inspire";
     private SharedPreferences sharedPreferences;
-    //private static final String PREF_NAME = "SHARED_PREFS";
+    private final String PREF_NAME = "SHARED_PREFS";
 
 
     public JobSchedulerService() {
     }
 
-    public JobSchedulerService(SharedPreferences sharedPreferences) {
-        quotesList = new ArrayList<>();
-        this.sharedPreferences = sharedPreferences;
-    }
-
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        new JobTask().execute();
-        return false;
+        new JobTask(this).execute(jobParameters);
+        return true;
     }
 
     @Override
@@ -50,20 +44,30 @@ public class JobSchedulerService extends JobService {
     }
 
 
-    private class JobTask extends AsyncTask<Void, Void, Void> {
+    private class JobTask extends AsyncTask<JobParameters, Void, JobParameters> {
+
+        private final JobService jobService;
+        private SharedPreferences sharedPreferences;
+        private SharedPreferences.Editor editor;
+
+        public JobTask(JobService jobService) {
+            this.jobService = jobService;
+            sharedPreferences = getApplicationContext().getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+            editor = sharedPreferences.edit();
+        }
+
         @Override
-        protected Void doInBackground(Void... voids) {
-            final SharedPreferences.Editor editor = sharedPreferences.edit();
+        protected JobParameters doInBackground(JobParameters... jobParameters) {
             ApiInterface apiInterface = ApiService.getRetrofit().create(ApiInterface.class);
             Call<QuoteObject> call = apiInterface.getQuote(CATEGORY);
             call.enqueue(new Callback<QuoteObject>() {
                 @Override
                 public void onResponse(Call<QuoteObject> call, Response<QuoteObject> response) {
-                    Toast.makeText(JobSchedulerService.this, "onResponse", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(JobSchedulerService.this, "onResponse", Toast.LENGTH_SHORT).show();
                     if (response.isSuccessful()) {
                         quotesList = response.body().getContents().getQuotesList();
                         Quotes quotes = quotesList.get(0);
-                        editor.putString("quote_title" , quotes.getQuote());
+                        editor.putString("quote_title", quotes.getQuote());
                         editor.putString("quote_url", quotes.getBackground());
                         editor.putString("error_message", null);
                         editor.apply();
@@ -82,31 +86,13 @@ public class JobSchedulerService extends JobService {
                     editor.apply();
                 }
             });
-            return null;
+            return jobParameters[0];
         }
 
+        @Override
+        protected void onPostExecute(JobParameters jobParameters) {
+            super.onPostExecute(jobParameters);
+            jobService.jobFinished(jobParameters, false);
+        }
     }
-
-//
-//    public void fetchData() {
-//        ApiInterface apiInterface = ApiService.getRetrofit().create(ApiInterface.class);
-//        Call<QuoteObject> call = apiInterface.getQuote(CATEGORY);
-//        call.enqueue(new Callback<QuoteObject>() {
-//            @Override
-//            public void onResponse(Call<QuoteObject> call, Response<QuoteObject> response) {
-//                if (response.isSuccessful()) {
-//                    quotesList = response.body().getContents().getQuotesList();
-//                    Toast.makeText(getApplicationContext(), "success " + quotesList.size(), Toast.LENGTH_SHORT).show();
-//
-//                } else {
-//                    Toast.makeText(getApplicationContext(), "failure", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<QuoteObject> call, Throwable t) {
-//                Toast.makeText(getApplicationContext(), "" + t.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
 }
